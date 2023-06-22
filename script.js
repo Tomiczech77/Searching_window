7b80f87472fe521c0f08c7ef2b64f5ab051d76f7
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-06-22 22:35:39 +0200

    searching is ready

---
+++ script.js
@@ -0,0 +1,45 @@
+const input = document.querySelector(".input");
+const search = document.querySelector(".search");
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function(){
+    search.classList.toggle("active");
+    input.focus();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
